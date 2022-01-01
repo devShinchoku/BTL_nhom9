6bae5623a8e54974095842048d48fa0b7d31d131
kevilphu <1951060916@e.tlu.edu.vn>
2022-01-01 16:20:29 +0700

    hide-info-customer-js

---
+++ booking/js/booking.js
@@ -0,0 +1,11 @@
+//click radio hide content
+
+$(document).ready(function() {
+    $("#id_delivery_0").click(function() {
+        $(".formInput").show();
+    });
+    $("#id_delivery_2").click(function() {
+        $(".formInput").hide();
+    });
+});
+
