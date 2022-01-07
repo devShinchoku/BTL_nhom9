4a1a9b523dc4259e7eeba4e93d76cba4d6b43fb2
Huy Nam <1951060882@e.tlu.edu.vn>
2022-01-07 22:45:00 +0700

    update

--- accounts/sign-up/sign-up.js
+++ accounts/sign-up/sign-up.js
@@ -10,22 +10,10 @@ $(document).ready(function(){
             $("#sr_pass1").text("Password ok").css("color","black");       
     })
 })
-// // check dieu kien pass
-// $(document).ready(function(){
-//     $("#inputPassword1").change(function(){
-//         let passPattern =/(?=.[a-z])(?=.[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
-//         if(passPattern.test($(this).val())){
-//             $("#passlHelp").text("Password hop le").css("color","red");                    
-//         }
-//         else{
-//             $("#passlHelp").text("Password khong hop le").css("color","red");
-//         }
-//     })
-// })
-// check dieu kien pass
+
 $(document).ready(function(){
     $("#inputPassword1").change(function(){
-        let passPattern =/^(?=.[a-z])(?=.[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+        let passPattern =/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[ -/:-@\[-`{-~]).{6,64}$/;
         if(passPattern.test($(this).val()) == false){
             $("#passlHelp").text("Password khong hop le").css("color","red");
         }
